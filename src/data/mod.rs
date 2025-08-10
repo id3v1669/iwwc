fa497748f21751261fde33ba795592095ea7d80e
id3v1669 <git@id3v1669.com>
2025-08-10 18:21:07 +0800

    icons: ref

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,4 +1,4 @@
 pub mod config;
 pub mod icons;
 pub mod notification;
-pub mod shared;
+pub mod variables;
--- src/data/shared.rs
+++
@@ -1,6 +0,0 @@
-use once_cell::sync::Lazy;
-use std::collections::HashMap;
-use std::sync::Mutex;
-
-pub static ICONS: Lazy<Mutex<HashMap<String, std::path::PathBuf>>> =
-    Lazy::new(|| Mutex::new(crate::data::icons::get_system_icons_paths()));
